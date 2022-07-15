c45c9ba725b132e28195b78f62f9a794aa20a9a2
Kamaal <kamaal.f1@gmail.com>
2022-07-15 21:18:41 +0200

    Adding boilerplate configuration files

---
+++ Scripts/pre_compile.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#  pre_compile.sh
+#  Tasktive
+#
+#  Created by Kamaal M Farah on 15/07/2022.
+#  
+
+if which swiftformat >/dev/null; then
+    swiftformat .
+else
+    echo "warning: SwiftFormat not installed, download from https://github.com/nicklockwood/SwiftFormat"
+fi
+
+if which swiftlint >/dev/null; then
+    swiftlint
+else
+    echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
+fi
