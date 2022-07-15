82284a271d892af95a39305fbf2379099628d350
Kamaal <kamaal.f1@gmail.com>
2022-07-15 21:32:42 +0200

    Designing task model

---
+++ Tasktive/Models/CoreData/CoreTask/CoreTask+CoreDataClass.swift
@@ -0,0 +1,13 @@
+//
+//  CoreTask+CoreDataClass.swift
+//  Tasktive
+//
+//  Created by Kamaal M Farah on 15/07/2022.
+//
+//
+
+import CoreData
+import Foundation
+
+@objc(CoreTask)
+public class CoreTask: NSManagedObject { }
---
+++ Tasktive/Models/CoreData/CoreTask/CoreTask+CoreDataProperties.swift
@@ -0,0 +1,27 @@
+//
+//  CoreTask+CoreDataProperties.swift
+//  Tasktive
+//
+//  Created by Kamaal M Farah on 15/07/2022.
+//
+//
+
+import CoreData
+import Foundation
+
+extension CoreTask {
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<CoreTask> {
+        NSFetchRequest<CoreTask>(entityName: "CoreTask")
+    }
+
+    @NSManaged public var id: UUID
+    @NSManaged public var title: String
+    @NSManaged public var taskDescription: String?
+    @NSManaged public var notes: String?
+    @NSManaged public var creationDate: Date
+    @NSManaged public var updateDate: Date
+    @NSManaged public var ticked: Bool
+    @NSManaged public var dueDate: Date
+}
+
+extension CoreTask: Identifiable { }
