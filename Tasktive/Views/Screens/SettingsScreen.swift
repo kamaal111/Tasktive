0792bbcb84099637fee0a597f936c5e36374c981
Kamaal Farah <kfarah@box.com>
2022-07-24 21:23:16 +0200

    Showing current app version in settings screen

--- Tasktive/Views/Screens/SettingsScreen.swift
+++ Tasktive/Views/Screens/SettingsScreen.swift
@@ -6,19 +6,51 @@
 //
 
 import SwiftUI
+import SalmonUI
+import SettingsUI
 
 private let SCREEN: NamiNavigator.Screens = .settings
 
 struct SettingsScreen: View {
+    @StateObject private var viewModel = ViewModel()
+
     var body: some View {
         ScreenWrapper(screen: SCREEN) {
-            Text("Settings")
+            Form {
+                // - TODO: LOCALIZE THIS
+                Section(header: Text("About")) {
+                    SettingsUI.VersionRowView()
+                }
+            }
+            // - TODO: LOCALIZE THIS
+            Text("Made by Kamaal")
+                .foregroundColor(.secondary)
+                .ktakeSizeEagerly(alignment: .bottom)
+        }
+    }
+}
+
+extension SettingsScreen {
+    final class ViewModel: ObservableObject {
+        var versionText: String? {
+            Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
+        }
+
+        var buildNumber: String? {
+            Bundle.main.infoDictionary?["CFBundleVersion"] as? String
         }
     }
 }
 
 struct SettingsScreen_Previews: PreviewProvider {
     static var previews: some View {
-        SettingsScreen()
+        var configuration = PreviewConfiguration()
+        configuration.screen = .settings
+
+        return ForEach(ColorScheme.allCases, id: \.self) { scheme in
+            MainView()
+                .previewEnvironment(withConfiguration: configuration)
+                .colorScheme(scheme)
+        }
     }
 }
