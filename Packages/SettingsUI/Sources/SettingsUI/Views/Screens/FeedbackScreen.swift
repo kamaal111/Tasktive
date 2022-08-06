d5b5b703157979015a6ed6e32f091ca3a85ba95f
Kamaal <kamaal.f1@gmail.com>
2022-08-06 18:50:16 +0200

    Designing feedback screen

--- Packages/SettingsUI/Sources/SettingsUI/Views/Screens/FeedbackScreen.swift
+++ Packages/SettingsUI/Sources/SettingsUI/Views/Screens/FeedbackScreen.swift
@@ -6,9 +6,14 @@
 //
 
 import SwiftUI
+import SalmonUI
 
 extension SettingsUI {
     public struct FeedbackScreen: View {
+        @Environment(\.colorScheme) private var colorScheme
+
+        @StateObject private var viewModel = ViewModel()
+
         public let style: FeedbackStyles
 
         public init(style: FeedbackStyles) {
@@ -16,11 +21,72 @@ extension SettingsUI {
         }
 
         public var body: some View {
-            Text(style.title)
-                .navigationTitle(Text(NSLocalizedString("Feedback", bundle: .module, comment: "")))
+            VStack {
+                KFloatingTextField(
+                    text: $viewModel.title,
+                    title: NSLocalizedString("Title", bundle: .module, comment: "")
+                )
+                KTextView(
+                    text: $viewModel.description,
+                    title: NSLocalizedString("Description", bundle: .module, comment: "")
+                )
+                Button(action: onSendPress) {
+                    Text(NSLocalizedString("Send", bundle: .module, comment: ""))
+                        .font(.headline)
+                        .bold()
+                        .foregroundColor(colorScheme == .dark ? .black : .white)
+                        .padding(.vertical, 4)
+                        .ktakeWidthEagerly()
+                        .background(Color.accentColor)
+                        .cornerRadius(8)
+                }
+                .buttonStyle(.plain)
+                .disabled(viewModel.disableSubmit)
+            }
+            .padding(.vertical, 16)
+            .padding(.horizontal, 16)
+            .navigationTitle(Text(style.title))
             #if os(iOS)
                 .navigationBarTitleDisplayMode(.inline)
             #endif
+        }
+
+        private func onSendPress() {
+            Task {
+                await viewModel.submit()
+            }
+        }
+    }
+}
+
+extension SettingsUI.FeedbackScreen {
+    final class ViewModel: ObservableObject {
+        @Published var title = ""
+        @Published var description = ""
+        @Published var loading = false
+
+        init() { }
+
+        var disableSubmit: Bool {
+            loading || title.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
+        }
+
+        func submit() async {
+            await withLoading(completion: {
+                #warning("Handle submit logic")
+            })
+        }
+
+        private func withLoading<T>(completion: () -> T) async -> T {
+            await setLoading(true)
+            let result = completion()
+            await setLoading(false)
+            return result
+        }
+
+        @MainActor
+        private func setLoading(_ state: Bool) {
+            loading = state
         }
     }
 }
