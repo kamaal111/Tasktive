5c01779ec25590f1c33c0efff3d64c3616ad2b1a
Kamaal <kamaal.f1@gmail.com>
2022-08-14 20:32:39 +0200

    Documenting FeedbackConfiguration

--- Packages/SettingsUI/Sources/SettingsUI/Models/Structures/FeedbackConfiguration.swift
+++ Packages/SettingsUI/Sources/SettingsUI/Models/Structures/FeedbackConfiguration.swift
@@ -7,13 +7,26 @@
 
 import Foundation
 
+/// Configuration for the feedback row in the settings screen.
 public struct FeedbackConfiguration {
+    /// This token will be used to create issues in GitHub.
     public let gitHubToken: String
+    /// The username or organization name of the repo where we will create the issue on.
     public let gitHubUsername: String
+    /// The repo name where we create the issue on.
     public let repoName: String
+    /// Some extra data to send with the issue.
     public let additionalFeedbackData: Encodable
+    /// Extra labels on the issue that will be created.
     public let additionalIssueLabels: [String]
 
+    /// Main initializer for FeedbackConfiguration.
+    /// - Parameters:
+    ///   - gitHubToken: This token will be used to create issues in GitHub.
+    ///   - gitHubUsername: The username or organization name of the repo where we will create the issue on.
+    ///   - repoName: The repo name where we create the issue on.
+    ///   - additionalFeedbackData: Some extra data to send with the issue.
+    ///   - additionalIssueLabels: Extra labels on the issue that will be created.
     public init(
         gitHubToken: String,
         gitHubUsername: String,
