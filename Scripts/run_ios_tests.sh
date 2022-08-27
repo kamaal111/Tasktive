a4d3aa717f33201035a382b91db2614c8ccb10aa
Kamaal <kamaal.f1@gmail.com>
2022-08-27 17:47:48 +0200

    Implementing ios test script

--- Scripts/run_ios_tests.sh
+++ Scripts/run_ios_tests.sh
@@ -6,4 +6,31 @@
 #  Created by Kamaal M Farah on 27/08/2022.
 #  
 
-echo "test"
+PROJECT="Tasktive.xcodeproj"
+
+iOS_destinations=(
+  "platform=iOS Simulator,name=iPhone 13 Pro Max"
+)
+
+iOS_test_schemes=(
+  "Tasktive"
+)
+
+xcode_test() {
+    set -o pipefail && xcodebuild test -project "$PROJECT" -scheme "$1" -destination "$2" | bundle exec xcpretty || exit 1
+}
+
+test_all_destinations() {
+  time {
+      for destination in "${iOS_destinations[@]}"
+      do
+        for scheme in "${iOS_test_schemes[@]}"
+        do
+        echo "testing $scheme on $destination"
+        xcode_test "$scheme" "$destination"
+        done
+    done
+  }
+}
+
+test_all_destinations
