9842481716d5bd01efbc704bee6c5596a39e21c1
Kamaal <kamaal.f1@gmail.com>
2022-08-27 21:33:00 +0200

    Updating support author screen for older versions

--- Packages/SettingsUI/Sources/SettingsUI/Views/Screens/SupportAuthorScreen.swift
+++ Packages/SettingsUI/Sources/SettingsUI/Views/Screens/SupportAuthorScreen.swift
@@ -10,21 +10,10 @@ import SwiftUI
 import SalmonUI
 import ConfettiSwiftUI
 
-@available(macOS 13.0, iOS 16.0, *)
-private let logger = Logger(
-    subsystem: "io.kamaal.SettingsUI",
-    category: String(describing: SettingsUI.SupportAuthorScreen.self)
-)
-
 extension SettingsUI {
+    #if swift(>=5.7)
     @available(macOS 13.0, iOS 16.0, *)
     public struct SupportAuthorScreen: View {
-        @EnvironmentObject private var store: Store
-
-        @State private var confettiTimesRun = 0
-        @State private var numberOfConfettis = 20
-        @State private var confettiRepetitions = 0
-
         @Binding public var navigationPath: NavigationPath
 
         let handlePurchaseFailure: (_ error: Store.Errors) -> Void
@@ -36,6 +25,47 @@ extension SettingsUI {
         }
 
         public var body: some View {
+            SupportAuthorScreenView(
+                handlePurchaseFailure: handlePurchaseFailure,
+                navigateBack: { navigationPath.removeLast() }
+            )
+        }
+    }
+    #else
+    public struct SupportAuthorScreen: View {
+        @Environment(\.presentationMode) private var presentationMode
+
+        let handlePurchaseFailure: (_ error: Store.Errors) -> Void
+
+        public init(handlePurchaseFailure: @escaping (_ error: Error) -> Void) {
+            self.handlePurchaseFailure = handlePurchaseFailure
+        }
+
+        public var body: some View {
+            SupportAuthorScreenView(
+                handlePurchaseFailure: handlePurchaseFailure,
+                navigateBack: { presentationMode.wrappedValue.dismiss() }
+            )
+        }
+    }
+    #endif
+
+    private struct SupportAuthorScreenView: View {
+        private let logger = Logger(
+            subsystem: "io.kamaal.SettingsUI",
+            category: "SupportAuthorScreenView"
+        )
+
+        @EnvironmentObject private var store: Store
+
+        @State private var confettiTimesRun = 0
+        @State private var numberOfConfettis = 20
+        @State private var confettiRepetitions = 0
+
+        let handlePurchaseFailure: (_ error: Store.Errors) -> Void
+        let navigateBack: () -> Void
+
+        var body: some View {
             ScrollView(.vertical, showsIndicators: true) {
                 ZStack {
                     VStack {
@@ -81,16 +111,6 @@ extension SettingsUI {
             })
         }
 
-        private func shootConfetti(for donation: CustomProduct) {
-            let weight = donation.weight
-
-            DispatchQueue.main.async {
-                numberOfConfettis = (20 * weight)
-                confettiRepetitions = weight < 1 ? 0 : (weight - 1)
-                confettiTimesRun += 1
-            }
-        }
-
         private func handleAppear() {
             Task {
                 let result = await store.requestProducts()
@@ -101,11 +121,20 @@ extension SettingsUI {
                         "error='\(failure)'",
                     ].joined(separator: ";")
                     logger.error("\(message)")
-                    // Navigate back
-                    navigationPath.removeLast()
+                    navigateBack()
                 case .success:
                     break
                 }
+            }
+        }
+
+        private func shootConfetti(for donation: CustomProduct) {
+            let weight = donation.weight
+
+            DispatchQueue.main.async {
+                numberOfConfettis = (20 * weight)
+                confettiRepetitions = weight < 1 ? 0 : (weight - 1)
+                confettiTimesRun += 1
             }
         }
     }
