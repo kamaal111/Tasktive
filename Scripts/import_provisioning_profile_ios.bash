beae051aa748224c32a924286d95dcd2d377c702
Kamaal <kamaal.f1@gmail.com>
2022-09-11 17:21:50 +0200

    Uploading app to app store connect

--- Scripts/import_provisioning_profile_ios.bash
+++ Scripts/import_provisioning_profile_ios.bash
@@ -10,3 +10,7 @@ set -euo pipefail
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 echo "$PROVISIONING_PROFILE_DATA" | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/profile.mobileprovision
+
+ls ~/Library/MobileDevice/Provisioning\ Profiles
+
+echo "Provisioning profile created"
