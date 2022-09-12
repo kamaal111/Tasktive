285a3c254de743b0eadf047f7fe1097f5268123e
Kamaal <kamaal.f1@gmail.com>
2022-09-12 22:57:18 +0200

    Completing deployment job

--- Scripts/archive_ios.sh
+++ Scripts/archive_ios.sh
@@ -7,14 +7,5 @@
 #
 
 SCHEME="Tasktive"
-WORKSPACE="Tasktive.xcworkspace"
-PROJECT_NAME="Tasktivity"
-ARCHIVE_PATH="Archive/$PROJECT_NAME.xcarchive"
 
 bundle exec fastlane gym --scheme $SCHEME
-
-# mkdir -p Archive
-
-# xcodebuild -workspace $WORKSPACE -scheme $SCHEME archive -configuration release -sdk iphoneos -archivePath $ARCHIVE_PATH
-
-# xcodebuild -exportArchive -archivePath $ARCHIVE_PATH -exportPath "Archive/$PROJECT_NAME.ipa" -exportOptionsPlist fastlane/ExportOptions.plist
