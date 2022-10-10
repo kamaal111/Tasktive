e6d7922e307e575dd4b8bd27fec23a9aca1ac7dc
Kamaal <kamaal.f1@gmail.com>
2022-10-10 16:34:22 +0200

    Fixing macOS build

--- Modules/SettingsUI/Package.swift
+++ Modules/SettingsUI/Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         .package(url: "https://github.com/kamaal111/StoreAPIClient.git", "0.1.1" ..< "0.2.0"),
         .package(url: "https://github.com/simibac/ConfettiSwiftUI.git", "1.0.1" ..< "1.0.2"),
         .package(url: "https://github.com/kamaal111/ShrimpExtensions.git", "2.6.0" ..< "3.0.0"),
-        .package(url: "https://github.com/kamaal111/InAppBrowserSUI.git", "1.0.0" ..< "2.0.0"),
+        .package(url: "https://github.com/kamaal111/InAppBrowserSUI.git", "2.0.0" ..< "3.0.0"),
         .package(path: "../Environment"),
         .package(path: "../Logster"),
         .package(path: "../TasktiveLocale"),
